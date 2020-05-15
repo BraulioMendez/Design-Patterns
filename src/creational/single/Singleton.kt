0aa2ab3f3a15ebc77b1a6af5d61d43faee8d01c4
Braulio Mendez <brauliomendez@Braulios-MacBook-Pro.local>
2020-05-15 02:05:49 -0500

    Singleton pattern was created.

---
+++ src/creational/single/Singleton.kt
@@ -0,0 +1,8 @@
+object Singleton {
+
+    fun showSingletonInformation(): String {
+        return "A Singleton is a software design pattern that guarantees a class has one instance only and a global point of access to it is provided by that class.\n" +
+                "Singleton Pattern ensures that only one instance would be created and it would act as a single point of access thereby ensuring thread safety."
+    }
+
+}
